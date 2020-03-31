be2fe1d9ed3f0924fa6fd8b72ab1e287b65a27a3
Aaron Cordova <theqodesmith@gmail.com>
2020-03-31 17:47:19 -0400

    Naive search working.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { createWordTree } from 'helpers'
+import allWords from '../../all-words.json'
+
+const english = Object.keys(allWords).reduce((acc, letter) => {
+  const list = allWords[letter]
+  createWordTree(list, acc)
+  return acc
+}, {})
+
+function getListOfWords(searchTerm) {
+  return searchTerm.split('').reduce((acc, letter, i) => {
+    if (!acc || !acc[letter]) return null
+    if (i === searchTerm.length - 1) {
+      return acc[letter].words
+    } else {
+      return acc[letter]
+    }
+  }, english)
+}
+
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [currentList, setCurrentList] = useState([])
+
+  useEffect(() => {
+    let list
+    if (searchTerm) list = getListOfWords(searchTerm)
+    setCurrentList(list || [])
+  }, [searchTerm])
+
   return (
-    <>
-      <header className="pv5 bg-gold black-80">
-        <h1 className="mt0 mb1 tc">Create New App</h1>
-        <div className="tc ttc">by the Qodesmith</div>
-      </header>
-      <div className="pt4 pb1 tc">Go save the world with JavaScript</div>
-      <div className="tc">and edit <code>src/components/<span className="b">App.jsx</span></code>!</div>
-    </>
+    <div className="h-100 pa5 df flex-col">
+      <h1 className="pv5 mv0 mha">Search For A Word</h1>
+
+      <input
+        className="input-search mha"
+        type="text"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+
+      <div className="search-results mt5 overflow-y-a pa2 mha">
+        {
+          currentList.length === 0 ? 'No results.' : (
+            currentList.map(word => <div key={word}>{word}</div>)
+          )
+        }
+      </div>
+    </div>
   )
 }
 
